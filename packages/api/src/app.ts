711cbb4274844cdbc5054e8aa906cb44649c3af0
Nicolas Theck <contact@itsrems.com>
2021-07-07 15:27:58 +0200

    dev scripts & windows hotfix

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,11 +1,22 @@
-import { PrismaClient } from '@prisma/client';
-import { App } from 'uWebSockets.js';
+import { App, DEDICATED_COMPRESSOR_3KB } from 'uWebSockets.js';
 
-const prisma = new PrismaClient();
+import { builder, Context } from './builder';
+
+const schema = builder.toSchema({});
 
 const server = App();
 
 async function main() {
+  server.ws('/graphql', {
+    idleTimeout: 32,
+    maxBackpressure: 1024,
+    maxPayloadLength: 512,
+    compression: DEDICATED_COMPRESSOR_3KB,
+    
+    message: (ws, msg, isBinary) => {
+      console.log(msg);
+    }
+  });
   server.listen(1500, (socket) => {
     if (socket) console.log(`Up and running`, socket);
   });
