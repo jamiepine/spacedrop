16431d57103c3f86b50b80afc853733f4ac90625
Jamie <ijamespine@me.com>
2021-07-13 17:36:06 -0700

    responsive mods

--- packages/landing/src/data/faq.ts
+++ packages/landing/src/data/faq.ts
@@ -4,11 +4,11 @@ export default [
     a: `Absolutely, you can set your preferred keybind from CTRL + Space to whatever you want in the settings.`
   },
   {
-    q: `Can I change the default keybind?`,
-    a: `Absolutely, you can set your preferred keybind from CTRL + Space to whatever you want in the settings.`
+    q: `How fast are transfer speeds?`,
+    a: `About as fast as your internet connection can handle, we have a network of servers worldwide that find the fastest path to stream data between clients.`
   },
   {
-    q: `Can I change the default keybind?`,
-    a: `Absolutely, you can set your preferred keybind from CTRL + Space to whatever you want in the settings.`
+    q: `What is the Signal Protocol?`,
+    a: `The Signal Protocol is an encrypt`
   }
 ];
